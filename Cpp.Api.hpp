82f19604d543791f30c565613cccb55f0de71e07
James Schumacher <james_schumacher_california@outlook.com>
2017-07-06 16:32:10 -0700

    Update Cpp.Api.hpp

--- Cpp.Api.hpp
+++ Cpp.Api.hpp
@@ -3,9 +3,24 @@
 
 namespace CppApi
 {
+    
     template <typename IntType> class TInteger
     {
     public:
+        enum IntegerOperation : unsigned long
+        {
+            Multiply = 1,
+            Divide = 2,
+            Modulus = 3,
+            Add = 4,
+            Subtract = 5,
+            AND = 6,
+            OR = 7,
+            XOR = 8,
+            LeftShift = 9,
+            RightShift = 10
+        }
+        
         inline TInteger() : mv_Value()
         {
         }
@@ -16,6 +31,12 @@ namespace CppApi
         
         inline TInteger(const TInteger & valObj) : mv_Value(valObj.mv_Value)
         {
+        }
+        
+        inline TInteger(const TInteger & val1, const TInteger & val2,
+                        IntegerOperation enOpCode)
+        {
+            
         }
     protected:
         IntType mv_Value;
